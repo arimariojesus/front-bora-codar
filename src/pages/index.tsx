6bc67f4aca589ef886c9610689034939ac7169a4
Ari Jesus <arimario.jesus@hotmail.com>
2021-07-06 22:26:39 -0300

    fix: provisional changes to build

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -116,31 +116,44 @@ export default function Home({ lastEpisode, allEpisodes }: HomeProps) {
 
 export const getStaticProps: GetStaticProps = async () => {
   try {
-    const { data: episodes } = await api.get('episodes');
+    // const { data: episodes } = await api.get('episodes');
 
-    if (!episodes) {
-      return {
-        notFound: true,
-      };
-    }
+    // if (!episodes) {
+    //   return {
+    //     notFound: true,
+    //   };
+    // }
 
-    const mappedEpisodes = episodes.map(episode => {
-      return {
-        id: episode._id,
-        title: episode.title,
-        thumbnail: episode.thumbnail,
-        // members: episode.members,
-        // publishedAt: format(parseISO(episode.published_at), 'd MMM yy', { locale: ptBR }),
-        slug: episode.slug,
-        publishedAt: episode.published_at,
-        duration: parseInt(episode.file.duration, 10),
-        durationAsString: convertDurationToTimeString(parseInt(episode.file.duration, 10)),
-        url: episode.file.url,
-      }
-    });
+    // const mappedEpisodes = episodes.map(episode => {
+    //   return {
+    //     id: episode._id,
+    //     title: episode.title,
+    //     thumbnail: episode.thumbnail,
+    //     // members: episode.members,
+    //     // publishedAt: format(parseISO(episode.published_at), 'd MMM yy', { locale: ptBR }),
+    //     slug: episode.slug,
+    //     publishedAt: episode.published_at,
+    //     duration: parseInt(episode.file.duration, 10),
+    //     durationAsString: convertDurationToTimeString(parseInt(episode.file.duration, 10)),
+    //     url: episode.file.url,
+    //   }
+    // });
+    const mappedEpisodes = [{
+      title: 'Série Código Limpo - EP 01 [prefácio]',
+      published_at: '2021-02-21 20:10:40',
+      thumbnail: 'https://production.listennotes.com/podcasts/bora-codar-RsLEMX_G8at-k7eaqr-ljOs.300x300.jpg',
+      description: '<p>Codigo limpo é uma verdadeira obra de arte, somos apaixonados pelo livro.</p><p>Esta série é uma declaração de amor a obra, onde uniremos o conteudo do livro com nossa vivencia de mercado; esperamos que gostem!</p><p>Nosso site:</p><p><a href=https://codar.app>https://codar.app</a></p><p>Link para comprar o livro na Amazon:</p><p><a href=https://www.amazon.com.br/s?k=codigo+limpo&adgrpid=81608350312&gclid=Cj0KCQiApsiBBhCKARIsAN8o_4h3bBlJ2CD74udp3pdT3x8fS2mBE6nARpqXUYzJzya7iKvpY2KumtgaAhegEALw_wcB&hvadid=426015455681&hvdev=c&hvlocphy=1001533&hvnetw=g&hvqmt=e&hvrand=1641774745756650072&hvtargid=kwd-447114230604&hydadcr=5622_11235117&tag=hydrbrgk-20&ref=pd_sl_4ujrlkbcg3_e>https://www.amazon.com.br/s?k=codigo+limpo&adgrpid=81608350312&gclid=Cj0KCQiApsiBBhCKARIsAN8o_4h3bBlJ2CD74udp3pdT3x8fS2mBE6nARpqXUYzJzya7iKvpY2KumtgaAhegEALw_wcB&hvadid=426015455681&hvdev=c&hvlocphy=1001533&hvnetw=g&hvqmt=e&hvrand=1641774745756650072&hvtargid=kwd-447114230604&hydadcr=5622_11235117&tag=hydrbrgk-20&ref=pd_sl_4ujrlkbcg3_e</a></p>',
+      // members: members,
+      // publishedAt: format(parseISO(published_at), 'd MMM yy', { locale: ptBR }),
+      slug: 'serie-codigo-limpo-ep-01-prefacio',
+      publishedAt: '',
+      duration: 0,
+      durationAsString: '',
+      url: 'https://d3ctxlq1ktw2nl.cloudfront.net/staging/2021-1-21/157518456-44100-2-1515b72064319.m4a',
+    }];
 
     const lastEpisode = mappedEpisodes[0];
-    const allEpisodes = mappedEpisodes.slice(1);
+    const allEpisodes = mappedEpisodes.slice(0);
 
     return {
       props: {
